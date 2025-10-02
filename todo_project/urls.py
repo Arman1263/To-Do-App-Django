76a96e98da974c8e6448f6798895572cb0db7fb1
Arman1263 <armanshikalgar01@gmail.com>
2025-10-02 17:38:53 +0530

    Remove login/logout and make app public

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -27,9 +27,4 @@ urlpatterns = [
     path('toggle/<int:task_id>/', views.toggle_task, name='toggle_task'),
     path('update/<int:task_id>/', views.update_task, name='update_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
-
-    # Auth
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
 ]
